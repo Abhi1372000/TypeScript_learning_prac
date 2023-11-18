410702559c28dd22caf75feddd38f12fe4bd9dab
Abhi1372000 <125535785+Abhi1372000@users.noreply.github.com>
2023-11-18 10:46:59 +0530

    some changes

--- src/index3_arrays.ts
+++ src/index3_arrays.ts
@@ -1,3 +1,5 @@
-let numbers: number[] = [1, 2, 3] // this is the type anotation for the array of numbers or we can write
+let numbers: number[] = [1, 2, 3]; // this is the type anotation for the array of numbers or we can write
 // let numbers = [1,2,3,4] like this
 // numbers.forEach(element => numbers. ); after the dot we can access the functions
+
+let fruits: string[] = ["apple", "mango", "kiwi"];
